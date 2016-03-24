9bd9da51a0c2c84b4de78def6db9c9d9c4ee8ebc
胡 开文 <Kevin@komatoMacBook-Air.local>
2016-03-24 20:03:42 +0800

    增加 用代码创建一个button

--- iOSDemo/ViewController.m
+++ iOSDemo/ViewController.m
@@ -13,6 +13,48 @@
 @end
 
 @implementation ViewController
+- (IBAction)creatButton:(UIButton *)sender {
+    
+    // 创建UIbutton
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    
+    
+    
+    //  设置文字
+    [btn setTitle:@"dianwoshishi" forState:UIControlStateNormal];
+    
+    
+    // 设置图片
+    UIImage *image = [UIImage imageNamed:@"btn_01"];
+    [btn setBackgroundImage:image forState:UIControlStateNormal];
+    
+    
+    
+    // 设置frame
+    btn.frame = CGRectMake(100, 0, 100, 100);
+    
+    
+    
+    // 修改按钮的文字颜色
+    [btn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    
+    
+    
+    // 添加到控制器的View上面
+    [self.view addSubview:btn];
+    
+    
+    
+    // 为按钮添加事件
+    [btn addTarget:self action:@selector(doSomeThing) forControlEvents:UIControlEventTouchUpInside];
+    
+    
+}
+-(void)doSomeThing{
+    
+    NSLog(@"点我干啥呢");
+
+}
 
 - (void)compute
 {
@@ -24,9 +66,7 @@
     
     [self.view endEditing:YES];
     
-    
-    
-    
+ 
     
     
     
